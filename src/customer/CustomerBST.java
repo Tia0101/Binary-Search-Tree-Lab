2319be12f17d14227a381622dab5e7d737b57258
Tia <tia.m.vanderyacht@gmail.com>
2024-05-28 20:39:08 -0700

    Comments added to CustomerBST class, print stament added to traversal methods for null values

--- src/customer/CustomerBST.java
+++ src/customer/CustomerBST.java
@@ -5,21 +5,25 @@ public class CustomerBST {
 
     private Node root;// root of the tree
 
+    // nested class to represent a node in the tree
     private class Node {
         protected Customer customer;
         protected Node left;
         protected Node right;
-
+        
+        // constructor
         public Node(Customer customer) {
             this.customer = customer;
         }
     }
 
+    // add a new customer to the tree
     public void addNode(Customer customer) {
         root = addHelper(root, customer);
         
     }
 
+    // helper method to add a new customer to the tree recursively
     private Node addHelper(Node current, Customer customer) {
         if (current == null) {
             return new Node(customer);
@@ -34,15 +38,17 @@ public class CustomerBST {
         return current;
     }
 
+    // remove a customer from the tree
     public void remove(int id) {
         root = removeHelper(root, id);
     }
 
+    // helper method to remove a customer from the tree recursively
     private Node removeHelper(Node current, int id) {
         if (current == null) {
             return null;
         }
-
+        // compare the id of the customer to the id of the current node
         if (id == current.customer.getId()) {
             // Node to delete found
             if (current.left == null && current.right == null) {
@@ -52,23 +58,24 @@ public class CustomerBST {
             } else if (current.left == null) {
                 return current.right;
             } else {
-                // node has two children
+                // node has two children replace the node with the smallest ID value in the right subtree
                 int smallestValue = findMinID(current.right);
                 current.customer.setId(smallestValue);
                 current.right = removeHelper(current.right, smallestValue);
                 return current;
             }
         } 
-
+        // if the id of the customer is less than the id of the current node, go to the left subtree
         if (id < current.customer.getId()) {
             current.left = removeHelper(current.left, id);
             return current;
         }
-
+        // if the id of the customer is greater than the id of the current node, go to the right subtree
         current.right = removeHelper(current.right, id);
         return current;
     }
-
+    
+    // helper method to find the smallest ID value in the tree recursively
     private int findMinID(Node root) {
         if (root.left == null) {
             return root.customer.getId();
@@ -77,19 +84,20 @@ public class CustomerBST {
         }
     }
 
+    // search for a customer in the tree
     public Customer search(int id) {
         return searchHelper(root, id);
     }
 
+    // helper method to search for a customer in the tree recursively
     private Customer searchHelper(Node current, int id) {
         if (current == null) {
             return null;
         }
-
+        // compare the id of the customer to the id of the current node
         if (id == current.customer.getId()) {
             return current.customer;
         }
-
         if (id < current.customer.getId()) {
             return searchHelper(current.left, id);
         } else {
@@ -97,11 +105,17 @@ public class CustomerBST {
         }
     }
 
+    // traverse the tree in-order
     public void traverseInOrder() {
         traverseInOrder(root);
     }
 
+    // helper method to traverse the tree in-order recursively
     private void traverseInOrder(Node node) {
+        if (node == null) {
+            System.out.println("There currently are no customers in the directory");;
+        }
+        //transverse left, root, right
         if (node != null) {
             traverseInOrder(node.left);
             System.out.print(node.customer.toString());
@@ -109,11 +123,17 @@ public class CustomerBST {
         }
     }
 
+    // traverse the tree pre-order
     public void traversePreOrder() {
             traversePreOrder(root);
         }
     
+    // helper method to traverse the tree pre-order recursively
     private void traversePreOrder(Node node) {
+        if (node == null) {
+            System.out.println("There currently are no customers in the directory");;
+        }
+        //transverse root, left, right
         if (node != null) {
             System.out.print(node.customer.toString());
             traversePreOrder(node.left);
@@ -121,32 +141,38 @@ public class CustomerBST {
         }
     }
 
+    // traverse the tree post-order
     public void traversePostOrder() {
         traversePostOrder(root);
     }
 
+    // helper method to traverse the tree post-order recursively
     private void traversePostOrder(Node node) {
+        if (node == null) {
+            System.out.println("There currently are no customers in the directory");;
+        }
+        //transverse left, right, root
         if (node != null) {
             traversePostOrder(node.left);
             traversePostOrder(node.right);
             System.out.print(node.customer.toString());
         }
     }
-
-        public void customerTraversalLookup(int traversalType) {
-            switch (traversalType) {
-                case 1:
-                    traversePreOrder();
-                    break;
-                case 2:
-                    traverseInOrder();
-                    break;
-                case 3:
-                    traversePostOrder();
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please enter 1, 2, or 3.");
-                    break;
-            }
+    // customerTraversalLookup method to allow the user to choose the type of traversal
+    public void customerTraversalLookup(int traversalType) {
+        switch (traversalType) {
+            case 1:
+                traversePreOrder();
+                break;
+            case 2:
+                traverseInOrder();
+                break;
+            case 3:
+                traversePostOrder();
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter 1, 2, or 3.");
+                break;
         }
+    }
 }
